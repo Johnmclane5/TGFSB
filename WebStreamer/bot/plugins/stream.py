96b283f78fc576a26310ce11759d32d9ccc0a14d
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2023-08-19 11:33:20 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -42,13 +42,10 @@ async def media_receive_handler(_, m: Message):
             log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
             file_hash = get_hash(log_msg, Var.HASH_LENGTH)
             stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
-            short_link = f"{Var.URL}{file_hash}{log_msg.id}"
-            logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
         try:
             await m.reply_text(
-                text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
-                    stream_link, short_link
-                ),
+                text="<code>{}</code>".format(
+                    stream_link),
                 quote=True,
                 parse_mode=ParseMode.HTML,
                 reply_markup=InlineKeyboardMarkup(
@@ -57,9 +54,8 @@ async def media_receive_handler(_, m: Message):
             )
         except errors.ButtonUrlInvalid:
             await m.reply_text(
-                text="<code>{}</code>\n\nshortened: {})".format(
-                stream_link, short_link
-            ),
+                text="<code>{}</code>".format(
+                stream_link),
             quote=True,
             parse_mode=ParseMode.HTML,
         )
